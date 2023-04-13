fea4aa254c87cde54c5ef5776726c656ce7fbb35
thasna2122 <56201386+thasna2122@users.noreply.github.com>
2023-04-13 09:47:45 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<bmoi.h>
+#include <mpi.h>
+
+main(int argc, char **argv) 
+    {
+      int ierr;
+
+      ierr = MPI_Init(&argc, &argv);
+      printf("Hello world\n"); 
+         
+      ierr = MPI_Finalize();
+   }
